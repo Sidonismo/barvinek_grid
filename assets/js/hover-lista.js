8d9e4b2e914809ef1e90e10fc90407ca10eff7c3
Eliáš Sidon <sidonismo@gmail.com>
2025-03-05 00:28:05 +0100

    uprava listy del media queries

--- assets/js/hover-lista.js
+++ assets/js/hover-lista.js
@@ -3,20 +3,37 @@ const lista = document.querySelectorAll('.lista')[0];
 
 console.log(lista);
 
-el.forEach(element => {
-    element.addEventListener(
-        "mouseover",
-        (event) => {
-            lista.style['z-index'] = '2';
-        },
-        false,
-      );
-      element.addEventListener(
-        "mouseout",
-        (event) => {
-            lista.style['z-index'] = '3';
-        },
-        false,
-      );
-});
+function myFunction(x) {
+    if (x.matches) { 
+        el.forEach(element => {
+            element.addEventListener(
+                "mouseover",
+                (event) => {
+                    lista.style['z-index'] = '2';
+                },
+                false,
+            );
+            element.addEventListener(
+                "mouseout",
+                (event) => {
+                    lista.style['z-index'] = '3';
+                },
+                false,
+            );
+        });
+    } else {
+      lista.style['z-index'] = '2';
+    }
+  }
+  
+  // Create a MediaQueryList object
+  var x = window.matchMedia("(min-width: 897px)")
+  
+  // Call listener function at run time
+  myFunction(x);
+  
+  // Attach listener function on state changes
+  x.addEventListener("change", function() {
+    myFunction(x);
+  }); 
 
